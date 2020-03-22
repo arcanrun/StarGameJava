9915c8ae41b317678cd371306bf5b861bd7f24dc
arcanrun <arcan.run@gmail.com>
2020-03-22 22:05:31 +0300

    init

---
+++ desktop/src/com/arcan/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.arcan.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.arcan.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
